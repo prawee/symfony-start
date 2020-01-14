b8a7d5c6a0546d0410f13b7ca88f20c6c1e125c8
Prawee Wongsa <prawee@hotmail.com>
2020-01-14 17:25:50 +0700

    how to add action to controller, render to view  and using params

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,18 +2,39 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * Class HomeController
+ * @package App\Controller
+ * @Route("/home", name="home")
+ */
 class HomeController extends AbstractController
 {
     /**
-     * @Route("/home", name="home")
+     * @Route("/index", name="index")
      */
     public function index()
     {
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+        ]);
+    }
+
+    /**
+     * @Route("/helloUser/{name?}", name="hello_user")
+     */
+    public function helloUser(Request $request, $name)
+    {
+        // request
+        //$name = $request->get('name');
+
+        //$form = $this->createFormBuilder();
+
+        return $this->render('home/greet.html.twig', [
+            'name' => $name
         ]);
     }
 }
